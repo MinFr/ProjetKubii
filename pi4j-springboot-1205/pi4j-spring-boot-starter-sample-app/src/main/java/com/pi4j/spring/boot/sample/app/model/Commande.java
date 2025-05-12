9874fdf485b32f9fa8f73bca34132274cffbb95f
MinFr <jiangmincongfr@163.com>
2025-05-12 17:35:35 +0200

    Add files via upload

---
+++ pi4j-springboot-1205/pi4j-spring-boot-starter-sample-app/src/main/java/com/pi4j/spring/boot/sample/app/model/Commande.java
@@ -0,0 +1,71 @@
+package com.pi4j.spring.boot.sample.app.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
+
+public class Commande {
+
+    private String _id;
+
+    private String bon_livraison;
+    private String date_du_controle;
+    private String client;
+    private String private_key;
+    
+    private Object commande_colis_articles;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getBon_livraison() {
+        return bon_livraison;
+    }
+
+    public void setBon_livraison(String bon_livraison) {
+        this.bon_livraison = bon_livraison;
+    }
+
+    public String getDate_du_controle() {
+        return date_du_controle;
+    }
+
+    public void setDate_du_controle(String date_du_controle) {
+        this.date_du_controle = date_du_controle;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public String getPrivate_key() {
+        return private_key;
+    }
+
+    public void setPrivate_key(String private_key) {
+        this.private_key = private_key;
+    }
+
+    public Object getCommande_colis_articles() {
+        return commande_colis_articles;
+    }
+
+    public void setCommande_colis_articles(Object commande_colis_articles) {
+        this.commande_colis_articles = commande_colis_articles;
+    }
+
+    
+    
+}
