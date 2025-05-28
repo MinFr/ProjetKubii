62bf739ab20ff27a619e3c1f4fb565aec22cbe6f
MinFr <jiangmincongfr@163.com>
2025-05-28 14:30:58 +0200

    Delete pi4j-springboot-1205 directory

--- pi4j-springboot-1205/pi4j-spring-boot/src/main/java/com/pi4j/spring/boot/ContextAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.pi4j.spring.boot;
-
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.Import;
-
-@AutoConfiguration
-@Import(ContextConfiguration.class)
-public class ContextAutoConfiguration {
-}
