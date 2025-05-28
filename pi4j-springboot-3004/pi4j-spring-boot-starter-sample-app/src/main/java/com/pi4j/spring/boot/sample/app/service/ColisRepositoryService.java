144c350d00886b8bff65a6037e4277f7f8a0274f
MinFr <jiangmincongfr@163.com>
2025-05-28 14:30:05 +0200

    Delete pi4j-springboot-3004 directory

--- pi4j-springboot-3004/pi4j-spring-boot-starter-sample-app/src/main/java/com/pi4j/spring/boot/sample/app/service/ColisRepositoryService.java
+++
@@ -1,8 +0,0 @@
-package com.pi4j.spring.boot.sample.app.service;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ColisRepositoryService extends MongoRepository<ColisService, String> {
-    // findByEtatColis 
-}
-
