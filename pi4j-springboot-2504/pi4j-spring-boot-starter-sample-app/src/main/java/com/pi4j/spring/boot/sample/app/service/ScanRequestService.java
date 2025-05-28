9886a1efa1be36d817de9fb9e5fe555ee37f8c4d
MinFr <jiangmincongfr@163.com>
2025-05-28 14:30:18 +0200

    Delete pi4j-springboot-2504 directory

--- pi4j-springboot-2504/pi4j-spring-boot-starter-sample-app/src/main/java/com/pi4j/spring/boot/sample/app/service/ScanRequestService.java
+++
@@ -1,9 +0,0 @@
-package com.pi4j.spring.boot.sample.app.service;
-
-public class ScanRequestService {
-    
-    public String id_colis;
-    public String ean13_emp;
-    public String ean13_product;
-
-}
